cc07bc3c8f1b51eacdf317ebb6e5fa68fa9c1dd7
AgendaToHeaven <vserranoortega@gmail.com>
2016-02-15 12:26:14 +0100

    First code with moveTowards method. Next try, calculate the desired position through a ecuation.

--- Assets/Scripts/StraightWayMeteor.cs
+++ Assets/Scripts/StraightWayMeteor.cs
@@ -3,13 +3,24 @@ using System.Collections;
 
 public class StraightWayMeteor : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public Vector3 end;
+    public float velocity;
+    private float distance;
+    private Vector3 desiredPosition;
+
+    void Awake()
+    {
+        velocity = 0.0f;   
+    }
+
+
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        transform.position = Vector3.MoveTowards(transform.position, end, Time.deltaTime * velocity);
+    }
 }
