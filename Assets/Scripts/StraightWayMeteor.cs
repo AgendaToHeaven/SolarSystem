5d52298a76b2cd6e936d035a59537e90df3341be
AgendaToHeaven <vserranoortega@gmail.com>
2016-02-25 09:38:58 +0100

    Meteor direction changes depend on the origin of the gravity. Simples modifications in the meteor way, moving the end point of it.

--- Assets/Scripts/StraightWayMeteor.cs
+++ Assets/Scripts/StraightWayMeteor.cs
@@ -1,16 +1,25 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class StraightWayMeteor : MonoBehaviour {
 
-    public Vector3 end;
+    public Transform end;
+    public Transform sun;
+    public Transform mercury;
+    public Transform venus;
+    public Transform earth;
+    public Transform mars;
+    public Transform jupiter;
+    public Transform saturn;
+    public Transform uranus;
+    public Transform neptune;
     public float velocity;
-    private float distance;
-    private Vector3 desiredPosition;
+    private float closeDistance;
+
 
     void Awake()
     {
-        velocity = 0.0f;   
+        velocity = 50.0f;
+        closeDistance = 1000f;
     }
 
 
@@ -21,6 +30,31 @@ public class StraightWayMeteor : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position = Vector3.MoveTowards(transform.position, end, Time.deltaTime * velocity);
+        /*targetDir = end.position - transform.position;
+        angle = Angle(targetDir);
+        if(angle > 100f)
+        {
+            transform.RotateAround(end.position, targetDir, velocity * Time.deltaTime);
+        }*/
+        // Gets a vector that points from the meteor's position to the target's and calculate distance.
+
+        Vector3 offset = sun.position - transform.position;
+        float sqrLen = offset.sqrMagnitude;
+        if (sqrLen < closeDistance * closeDistance)
+        {
+            print(sqrLen.ToString() + " 10^6 km. " + "The other transform is close to me and can attract me!");
+            if (end.position.x > transform.position.x)
+            {
+                print("Trayectoria modificada izquierda");
+                end.Translate(Vector3.left.normalized);
+            }
+            else
+            {
+                print("Trayectoria modificada derecha");
+                end.Translate(Vector3.right.normalized);
+            }
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, end.position, Time.deltaTime * velocity);
     }
 }
